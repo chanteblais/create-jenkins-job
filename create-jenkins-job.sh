a5161083d9b96d960967b9a6ae5fac24efc85b68
esg-cblais <cblais@eastsidegamestudio.com>
2020-08-28 13:26:08 -0700

    initial commit

--- create-jenkins-job.sh
+++ create-jenkins-job.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 source properties.env
-source secrets-exp.env || echo Error: missing secrets.env. Please create this file and try again.
+source secrets.env || echo Error: missing secrets.env. Please create this file and try again.
 
-if [[ $3 == 'gaia' ]]; then
-  echo "Creating project in Gaia"
-  source /Users/chanteblais/GitHub_Projects/create-jenkins-job/secrets-gaia.env || echo Error: missing secrets.env. Please create this file and try again.
-else
-  echo "Creating project in Exp"
-  source /Users/chanteblais/GitHub_Projects/create-jenkins-job/secrets.env || echo Error: missing secrets.env. Please create this file and try again.
-fi
-
-source /Users/chanteblais/GitHub_Projects/create-jenkins-job/properties.env
-
-JOB_CONFIG=`cat "/Users/chanteblais/GitHub_Projects/create-jenkins-job/templates/default-jenkins-job.xml" | \
+JOB_CONFIG=`cat "./templates/default-jenkins-job.xml" | \
         sed -e "s#{{ GIT_URL }}#$2#g" \
         -e "s#{{ GIT_BRANCH }}#$GIT_BRANCH#g" \
         -e "s#{{ SCRIPT_PREFIX }}#$SCRIPT_PREFIX#g"`
 
 echo "$JOB_CONFIG" > job-config.xml
-echo $GIT_URL
 
 java -jar $JENKINS_CLI_PATH -s $JENKINS_URL \
 -auth $J_USERNAME:$J_TOKEN \
